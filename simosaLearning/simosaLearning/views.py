acd0ba4432e8cdc3141bfc54a2b60a87da84343d
Afzal-Iqbal <afzallinkin@gmail.com>
2026-07-21 16:37:24 +0500

    templates in django with html & css

--- simosaLearning/simosaLearning/views.py
+++ simosaLearning/simosaLearning/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("hello, world. i am learning django for the second time")
+    #return HttpResponse("hello, world. i am learning django for the second time")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("This is about page of django page")
